c418fe855336db0b2c155da7789ad41912ce4a89
AdrianoBV <54295877+adrianobritovalenca@users.noreply.github.com>
2020-09-28 17:49:40 -0300

    AC02
    implementação da validação da senha

--- Aplicativo_Coel_OPE/app/src/main/java/br/com/coel/aplicativo_coel/MainActivity.kt
+++ Aplicativo_Coel_OPE/app/src/main/java/br/com/coel/aplicativo_coel/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.coel.aplicativo_coel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
@@ -17,7 +18,49 @@ class MainActivity : AppCompatActivity() {
         botaoLogin.setOnClickListener {
             val valorUsuario = campoUsuario.text.toString()
             val valorSenha = campoSenha.text.toString()
-            Toast.makeText(this, "Usuario $valorUsuario; Senha $valorSenha", Toast.LENGTH_LONG).show()
+            // Toast.makeText(this, "Usuario $valorUsuario; Senha $valorSenha", Toast.LENGTH_LONG).show()
+
+
+
+            //Validação do login
+            Thread {
+                // chama api de login enviando como parametro usuario e senha
+                var retornoLogin = false
+                val usuario = "aluno"
+                val senha = "impacta"
+
+                // verificação de usuario cadastrado
+                if (valorUsuario.equals(usuario,false)) {
+                    if (valorSenha.equals(senha,false)) {
+                        retornoLogin = true
+                    }
+                }
+                runOnUiThread {
+                    // depois que retorna da thread
+                    if (retornoLogin) {
+                        val intent = Intent(this, TelaInicialActivity::class.java) // Navega para TelaInicialActivity
+
+                        startActivity(intent)
+                    }
+                    else {
+                        Toast.makeText(this, "Login Inválido", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }.start()
+
+            /*
+            val intent = Intent(this, TelaInicialActivity::class.java) // Navega para TelaInicialActivity
+
+            //modo de enviar parametros para outra tela
+            val params = Bundle()
+            params.putString("nome_usuario", valorUsuario)
+            params.putInt("numero", 10)
+            intent.putExtras(params)
+            //modo de enviar parametros para outra tela
+             */
+
+            startActivity(intent)
+
         }
 
     }
