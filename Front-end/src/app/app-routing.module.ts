e20b809505e13cd84f0fa27ec793371cb3ffc455
AlexandreMarq <53923396+AlexandreMarq@users.noreply.github.com>
2020-10-12 20:00:17 -0300

    ...

--- Front-end/src/app/app-routing.module.ts
+++ Front-end/src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
-import { ModeloAntigoListarComponent } from './components/modelo-antigo-listar/modelo-antigo-listar.component';
-import { ConcorrentesListarComponent } from './components/concorrentes-listar/concorrentes-listar.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule, Component } from '@angular/core';
 
 import { AutenticacaoGuard } from './shared/autenticacao.guard';
 
+import { ModeloAntigoListarComponent } from './components/modelo-antigo-listar/modelo-antigo-listar.component';
+import { ConcorrentesListarComponent } from './components/concorrentes-listar/concorrentes-listar.component';
 import { ProdutosListarComponent } from './components/produtos-listar/produtos-listar.component';
 import { AutenticacaoComponent } from 'src/app/components/autenticacao/autenticacao.component';
+import { ProdutosCriarComponent } from './components/produtos-criar/produtos-criar.component';
 import { CriarContaComponent } from 'src/app/components/criar-conta/criar-conta.component';
 import { LoginComponent } from 'src/app/components/login/login.component';
 import { HomeComponent } from 'src/app/components/home/home.component';
-import { ProdutosCriarComponent } from './components/produtos-criar/produtos-criar.component';
 
 const routes: Routes = [
   {
